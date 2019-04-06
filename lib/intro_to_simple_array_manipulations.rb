46bcf30fe3c7a47baed4027512b1594d5eae4302
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-06 20:36:20 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,7 +16,21 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
-def using_concat(array)
+def using_concat(array, more_favs)
   array.concat(more_favs)
 end
-  
+def using_insert(array, element)
+  array.insert(4, element)
+end
+def using_uniq(array)
+  array.uniq!
+end
+def using_flatten(array)
+  array.flatten 
+end
+def using_delete(array, string)
+  array.delete(string) 
+end
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
