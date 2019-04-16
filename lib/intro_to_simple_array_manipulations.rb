7c8aba49811efde2174e32765a5ce3473748cb6f
github username <github email address>
2019-04-16 17:53:27 -0400

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,21 +22,26 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array, array1)
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
 end
 
-
-def describe using_insert()
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
-
-def describe using_uniq()
-
+def using_uniq(array)
+  array.uniq
 end
 
-
-describe using_delete()
+def using_flatten(array)
+  array.flatten
 end
 
-def describe using_delete_at()
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
